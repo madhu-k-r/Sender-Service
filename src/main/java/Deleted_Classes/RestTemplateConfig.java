88bf306dbc924e2c7f48a8d18acc89a493cfdcc7
madhu-k-r <120703199+madhu-k-r@users.noreply.github.com>
2023-05-15 15:03:03 +0530

    Update RestTemplateConfig.java

--- src/main/java/Deleted_Classes/RestTemplateConfig.java
+++ src/main/java/Deleted_Classes/RestTemplateConfig.java
@@ -1,15 +1,1 @@
-package Deleted_Classes;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
-@Configuration
-public class RestTemplateConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-}
 
